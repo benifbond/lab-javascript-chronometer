158afc96e86d5b0a469df8121803c4747e6b81df
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-05-09 16:14:33 +0200

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,63 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime =0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    
+  this.intervalId =  setInterval(() => {
+    this.currentTime += 1;
+if(callback)callback()
+
+} ,1000)
   }
 
   getMinutes() {
     // ... your code goes here
+
+return Math.floor(this.currentTime /60)
+
   }
 
   getSeconds() {
     // ... your code goes here
+    return (this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
-  }
+    // twoDigit = this.getMinutes + this.getSeconds
+    // if(twoDigit.length <= 2 ){
+    //   twoDigit
+    // } else{
+    //   return 
+    // }
+var twoDigit = value + "";
+if (twoDigit.length < 2) {
+  return "0" + twoDigit
+}else  {
+  return twoDigit
+}
+    }
+    
+  
 
   stop() {
-    // ... your code goes here
+    
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
-  }
+  this.currentTime =0;
+ }
 
   split() {
     // ... your code goes here
+    return `${ this.computeTwoDigitNumber (this.getMinutes(this.currentTime))}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}`
   }
-}
+  }
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
